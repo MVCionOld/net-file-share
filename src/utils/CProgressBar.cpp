6284134fce1cd359da39b1d786e1c8cd599bdda9
lhjlbjyjd <lhjlbjyjd@hmail.com>
2019-04-21 23:58:50 +0300

    Minor bug fixes

--- src/utils/CProgressBar.cpp
+++ src/utils/CProgressBar.cpp
@@ -14,13 +14,13 @@ void CProgressBar::PublishProgress (size_t current) {
   char current_time[ProgressBarVal::BUFFER_SIZE];
   sprintf(
       current_time,
-      "%02d:%02d:%02d",
+      "%02lu:%02lu:%02lu",
       time / 3600,
       (time / 60) % 60,
       time % 60
   );
   printf(
-      "[%-100s] (%d%%) (%d/%d) (Time spent: %s)\r",
+      "[%-100s] (%lu%%) (%lu/%lu) (Time spent: %s)\r",
       buffer,
       (current * 100) / total_,
       current, total_,
